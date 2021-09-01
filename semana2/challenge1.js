d1ab8b205fe784219afce83ed8bb6f7f95c7b989
oislasreyes <oislasreyes@gmail.com>
2021-08-31 22:36:48 -0500

    prueba

--- semana2/challenge1.js
+++ semana2/challenge1.js
@@ -1,5 +1,11 @@
 function twoSums(numbs, target) {
     //Implementación
+    return numbs.map((num1,i) => numbs.map((num2,j) => {
+        if (num1 + num2 === target && i !== j) {
+            return [i, j];
+        }
+    })).flat().filter((num) => num !== undefined)[0];
+
 }
 
 module.exports = twoSums;
